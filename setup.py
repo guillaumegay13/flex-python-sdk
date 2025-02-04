f674a4058eb58679075c8b6afda0dfba2c8b389b
Guillaume Gay <guillaume.gay@protonmail.com>
2025-02-04 14:50:51 +0100

    upgrade deprecated deployment

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='flex',
-    version='0.2',
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
-    install_requires=[
-        'requests'
-    ]
-)
+setup()
